83d1ba6a59193adebd499c31d8a3fa9d5a73a807
aviation5770 <aviation5770@gmail.com>
2026-08-28 14:04:54 +0900

    명령어 오류

--- src/bot/commands/ping.command.ts
+++ src/bot/commands/ping.command.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, MessageFlags } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const pingCommand = {
   data: new SlashCommandBuilder().setName("ping").setDescription("현재 지연 시간을 확인합니다."),
@@ -6,9 +6,8 @@ export const pingCommand = {
   async execute(interaction: ChatInputCommandInteraction) {
     const websocketLatency = Math.round(interaction.client.ws.ping);
 
-    await interaction.reply({
+    await interaction.editReply({
       content: `퐁! 현재 지연 시간은 ${websocketLatency}ms입니다.`,
-      flags: MessageFlags.Ephemeral,
     });
   },
 };
